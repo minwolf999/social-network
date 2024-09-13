b5f48bf601f069e55436278cb61ae33be0396b2a
anmarvin <minwolf.999@gmail.com>
2024-09-13 11:10:48 +0000

    Add: Verification if email is already in the db

--- Backend/Handler/Register.go
+++ Backend/Handler/Register.go
@@ -33,6 +33,18 @@ func Register(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		nw.Error("Internal Error: There is an Unmarshal error")
 		return
 	}
+	
+	// We get the row in the db where the email is equal to the email send
+	authData, err := utils.SelectFromDb("Auth", db, map[string]any{"Email": register.Auth.Email})
+	if err != nil {
+		nw.Error("Internal error: Problem during database query")
+		return
+	}
+
+	if len(authData) != 0 {
+		nw.Error("Email is already used")
+		return
+	}
 
 	// We insert in the table Auth of the db the id, email and password of the people trying to register
 	if err := utils.InsertIntoDb("Auth", db, register.Auth.Id, register.Auth.Email, register.Auth.Password); err != nil {
