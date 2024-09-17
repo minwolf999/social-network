4496c45739739dd8b0170e3537be76d714046c1a
anmarvin <minwolf.999@gmail.com>
2024-09-17 08:44:00 +0000

    Add: log folders and files

--- Backend/main.go
+++ Backend/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -13,6 +17,15 @@ import (
 
 func main() {
 	fmt.Println("\033[96mServer started at: http://localhost:8080\033[0m")
+
+	// We create a log file and redirect the stdout to the new file
+	logFile, _ := os.Create("./Log/" + time.Now().Format("2006-01-02__15-04-05") + ".log")
+	defer logFile.Close()
+
+	saveStdout := os.Stdout
+	os.Stdout = logFile
+
+	fmt.Println("Server started at: http://localhost:8080")
 
 	// We launch the server
 	mux := http.NewServeMux()
@@ -27,17 +40,37 @@ func main() {
 
 	// We set the time out limit
 	srv := &http.Server{
-		Handler: handler,
-		Addr:    "localhost:8080",
-
-		ReadHeaderTimeout: 15 * time.Second,
-		ReadTimeout:       15 * time.Second,
-		WriteTimeout:      10 * time.Second,
-		IdleTimeout:       30 * time.Second,
+		Handler:      handler,
+		Addr:         "localhost:8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
 	}
 
-	// We start the listening of the port
-	if err := srv.ListenAndServe(); err != nil {
-		fmt.Println(err)
+	go func() {
+		// We start the listening of the port
+		if err := srv.ListenAndServe(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	// Signal to capture a clean shutdown (SIGINT/SIGTERM)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	fmt.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// We close correctly the server
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %s\n", err)
 	}
+
+	// We reset the stdout to is normal status
+	fmt.Println("Server exiting")
+	os.Stdout = saveStdout
+	fmt.Println("Server exiting")
 }
