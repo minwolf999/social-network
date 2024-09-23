698c9d6ab75ae5a3590733dabd7d725c6b8b32bc
gbenard <gwendal.benard@gmail.com>
2024-09-23 13:18:32 +0000

    func change usrname & pas

--- Backend/Handler/ChangeUserData.go
+++ Backend/Handler/ChangeUserData.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	model "social-network/Model"
 	utils "social-network/Utils"
@@ -22,20 +23,45 @@ func ChangeUserName(db *sql.DB, name, uid string) error {
 		log.Println("Error Parsing Data", err)
 		return err
 	}
-	if err = bcrypt.CompareHashAndPassword([]byte(userdata.Username), []byte(name)); err != nil {
+
+	if userdata.Username == name {
 		log.Println("New Username and current Username are the same", err)
 		return err
 	} else {
-
+		utils.UpdateDb("UserInfos", db, map[string]any{"Username": name}, map[string]any{"Id": uid})
+		fmt.Println("Change username Succes")
 	}
 
 	return nil
 }
 
+func ChangePass(db *sql.DB, newpass, uid string) error {
+	actualpass, err := utils.SelectFromDb("UserInfo", db, map[string]any{"Id": uid})
+	if err != nil {
+		return err
+	}
 
+	userdata, err := ParseUserData(actualpass[0])
+	if err != nil {
+		log.Println("Error Parsing Data", err)
+		return err
+	}
 
+	if err = bcrypt.CompareHashAndPassword([]byte(userdata.Password), []byte(newpass)); err != nil {
+		log.Println("This Password is already used", err)
+		return err
+	} else {
+		utils.UpdateDb("Auth", db, map[string]any{"Password": newpass}, map[string]any{"Id": uid})
+		fmt.Println("Change password Succes")
+	}
+	return nil
+}
 
-
+/*
+if err = bcrypt.CompareHashAndPassword([]byte(userdata.Username), []byte(name)); err != nil {
+	log.Println("New Username and current Username are the same", err)
+	return err
+} else { */
 
 func ParseUserDataInfos(userData map[string]any) (model.Register, error) {
 	// We marshal the map to get it in []byte
