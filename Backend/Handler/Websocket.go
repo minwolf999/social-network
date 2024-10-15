54f33974d8591f9df545e8222e306a27d2157bbf
anmarvin <minwolf.999@gmail.com>
2024-10-15 11:30:20 +0000

    Add: init websocket route

--- Backend/Handler/Websocket.go
+++ Backend/Handler/Websocket.go
@@ -2,11 +2,27 @@ package handler
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func Websocket(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("[%s] [Websocket] %v", r.RemoteAddr, err)
+			return
+		}
 
+		for {
+			conn.WriteJSON("Hello world!")
+		}
 	}
 }
--- Backend/go.mod
+++ Backend/go.mod
@@ -5,5 +5,7 @@ go 1.23.1
 require (
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/mattn/go-sqlite3 v1.14.23
+	github.com/gorilla/websocket v1.5.3
 	golang.org/x/crypto v0.27.0
 )
+
