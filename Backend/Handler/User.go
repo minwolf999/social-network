5d399fe74a81ca36e3bbaa6c7e3e26ca21693dcc
anmarvin <minwolf.999@gmail.com>
2024-11-20 09:13:23 +0000

    Move functions for getAllUsers and his test function

--- Backend/Handler/User.go
+++ Backend/Handler/User.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -26,7 +27,7 @@ func GetUser(db *sql.DB) http.HandlerFunc {
 		// Create a temporary struct to hold the incoming request data.
 		var tmp struct {
 			// The JWT session ID for the user.
-			SessionId     string `json:"SessionId"`
+			SessionId string `json:"SessionId"`
 			// The ID of another user, if requested.
 			OtherPeopleId string `json:"OtherPeopleId"`
 		}
@@ -75,8 +76,8 @@ func GetUser(db *sql.DB) http.HandlerFunc {
 
 		// Encode the response JSON with user data.
 		err = json.NewEncoder(w).Encode(map[string]any{
-			"Success":   true,
-			"Message":   "Sending user info",
+			"Success": true,
+			"Message": "Sending user info",
 			// Return the retrieved user data.
 			"userInfos": userData,
 		})
@@ -87,3 +88,53 @@ func GetUser(db *sql.DB) http.HandlerFunc {
 	}
 }
 
+/*
+This function takes 1 argument:
+  - a pointer to an SQL database object
+
+The purpose of this function is to handle the retrieval of all the users in the database.
+
+The function returns an http.HandlerFunc that can be used as a handler for HTTP requests.
+*/
+func GetAllUsers(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		nw := model.ResponseWriter{
+			ResponseWriter: w,
+		}
+
+		var userJWT string
+		// Decode the JSON request body into the comment object
+		if err := json.NewDecoder(r.Body).Decode(&userJWT); err != nil {
+			// Send error if decoding fails
+			nw.Error("Invalid request body")
+			log.Printf("[%s] [GetAllUsers] Invalid request body: %v", r.RemoteAddr, err)
+			return
+		}
+
+		// Decrypt the OrganisatorId from the JWT to get the actual Organisator ID
+		_, err := utils.DecryptJWT(userJWT, db)
+		if err != nil {
+			nw.Error("Invalid JWT")
+			log.Printf("[%s] [GetAllUsers] Error during the decrypt of the JWT : %v", r.RemoteAddr, err)
+			return
+		}
+
+		var users model.Users
+		if err = users.SelectFromDb(db, map[string]any{}); err != nil {
+			fmt.Println(err)
+		}
+
+		// Set the response header to indicate JSON content and respond with success message.
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(map[string]any{
+			"Success": true,
+			"Message": "Users getted successfully",
+
+			"Users": users,
+		})
+		if err != nil {
+			// Log any error that occurs while encoding the response.
+			log.Printf("[%s] [GetAllUsers] %s", r.RemoteAddr, err.Error())
+		}
+	}
+}
