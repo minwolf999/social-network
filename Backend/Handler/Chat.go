307b30aac0840cc43639585c1ff59062eb5c141e
anmarvin <minwolf.999@gmail.com>
2024-12-10 10:25:56 +0000

    remove to mutch message in websocket of the sender of a group message

--- Backend/Handler/Chat.go
+++ Backend/Handler/Chat.go
@@ -221,18 +221,6 @@ func AddMessage(db *sql.DB) http.HandlerFunc {
 						log.Printf("[%s] [AddMessage] Error during the communication with the websocket : %s", r.RemoteAddr, err)
 						return
 					}
-
-					
-					_, isOk2 := model.ConnectedWebSocket.Conn[message.SenderId]
-					if isOk2 {
-						WebsocketMessage.Sender = message.ReceiverId
-						
-						if err = model.ConnectedWebSocket.Conn[message.SenderId].WriteJSON(WebsocketMessage); err != nil {
-							nw.Error("Error during the communication with the websocket")
-							log.Printf("[%s] [AddMessage] Error during the communication with the websocket : %s", r.RemoteAddr, err)
-							return
-						}
-					}
 				}
 				model.ConnectedWebSocket.Mu.Unlock()
 
